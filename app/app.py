55e25f137f112867da1fcb026d9f0880e45ee322
sai1907 <34799725+sai1907@users.noreply.github.com>
2020-08-26 22:34:24 -1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,9 +8,7 @@ import datetime
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 app.config["DEBUG"] = True
-status = {"status": "200 OK", "version": "0.0.1",
-       "uptime": "up since " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-
+status = {"uptime": "up since " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,9 +16,9 @@ def home():
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
+    status_version = {"status": "200 OK", "version": "0.0.1"}
+    status.update(status_version)
     return jsonify(status)
-
-
 
 if __name__ == '__main__':  # Script executed directly
     app.run('0.0.0.0', port=8080)
